55fe590b1e899aef3022d10dc5fd31edc85ce14e
Efrat Weksler <eweksler@g.jct.ac.il>
2023-02-28 12:56:27 +0200

    test

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,5 +1,30 @@
 package primitives;
 
-public class Point {
+public class Point 
+{
+	final Double3 xy z;
+	//add more stuff here perhaps
 
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+			if (obj instanceof Point other))
+			{
+				return xyz.equals(other.xyz);
+			}
+			return false;
+	}
+
+	@Override
+	public int hashCode() 
+	{
+		return xyz.hashCode();
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "" + xyz;
+	}
 }
