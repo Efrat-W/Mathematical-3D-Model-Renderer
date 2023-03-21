91cd42cfe4bf3bb0a7cc567c6b360007f8250390
Efrat Weksler <eweksler@g.jct.ac.il>
2023-03-21 18:02:57 +0200

    more javadoc

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -4,6 +4,11 @@ import primitives.Point;
 import primitives.Vector;
 
 public interface Geometry {
-
+	
+	/**
+	 * get normal vector, perpendicular at the point
+	 * @param p point on the geometric surface
+	 * @return normal vector
+	 */
 	public Vector getNormal(Point p);
 }
