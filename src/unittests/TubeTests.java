c471c443e0c93c0276558abcff8089847fa06c48
Efrat Weksler <eweksler@g.jct.ac.il>
2023-03-28 10:23:12 +0300

    beginning of tests creation

---
+++ src/unittests/TubeTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Efrat
+ *
+ */
+class TubeTests {
+
+	/**
+	 * Test method for {@link geometries.Tube#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
