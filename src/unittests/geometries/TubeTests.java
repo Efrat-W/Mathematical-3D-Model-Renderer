150770bd9aba0ed97ae0676b9ed63b13dcb5d959
Efrat Weksler <eweksler@g.jct.ac.il>
2023-04-24 14:46:37 +0300

    Tube tests for intersections

--- src/unittests/geometries/TubeTests.java
+++ src/unittests/geometries/TubeTests.java
@@ -4,6 +4,7 @@
 package unittests.geometries;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -52,5 +53,44 @@ class TubeTests {
 	 */
 	@Test
 	public void testFindIntersections() {
+	 Tube tube = new Tube(new Ray(new Point(0, -1, 0), new Vector(2,2,0)), 1);
+	 // ============ Equivalence Partitions Tests ==============
+	 // TC01: the ray starts from inside, intersects with the tube (1 point)
+	 Point p1 = new Point(1, 0, 1);
+	 List<Point> result1 = tube.findIntersections(new Ray(new Point(1,1,0),
+		    new Vector(0,-1,1)));
+	    assertEquals(1, result1.size(), "Wrong number of points");
+	    assertEquals(p1, result1.get(0), "Ray crosses tube from inside incorrectly.");
+	    
+	 // TC02: the ray starts from outside, intersects with the tube (2 point)
+	 List<Point> result2 = tube.findIntersections(new Ray(new Point(1.5, -0.5, 0),
+		    new Vector(0.2, 0.61, 0.7)));
+	    assertEquals(1, result2.size(), "Wrong number of points");
+	    assertEquals(p3, result2.get(0), "Ray crosses tube from outside incorrectly.");
+	    
+	 // TC03: the ray starts from outside, doesn't intersect with the tube (0 point)
+	 assertNull(Tube.findIntersections(new Ray(new Point(-1,1,1),
+		    new Vector(2,0,1))),
+		    "Ray out of tube");
+	    
+	    
+	 // =============== Boundary Values Tests ==================
+	 // TC11: the ray is parallel to the tube
+	 assertNull(Tube.findIntersections(new Ray(new Point(1,0,2),
+		    new Vector(1,1,0))),
+		    "Ray's parallel to tube");
+	 
+	// TC12: the ray is a skew line to the tube
+		 assertNull(Tube.findIntersections(new Ray(new Point(1,0,2),
+			    new Vector(1,1,0))),
+			    "Ray's parallel to tube, ray point on tube surface");
+	 
+	 // TC13: the ray is orthogonal to the tube
+	 assertNull(Tube.findIntersections(new Ray(new Point(1,0,1),
+		    new Vector(0,0,1))),
+		    "Ray's orthogonal to tube");
+	 
+	// TC12: the ray is a skew line to the tube, never intersecting
+	    
 	}
 }
